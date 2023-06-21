ee36dc2efd866eed9cc82af3b5f3f6abd9fba6da
Saryuns <sarahayunilams@gmail.com>
2023-06-21 13:40:58 +0700

    Initial commit

---
+++ API/Models/Booking.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+[Table("tb_m_bookings")]
+
+public class Booking
+{
+    [Key]
+    [Column("guid")]
+    public Guid Guid { get; set; }
+
+    [Column("start_date")]
+    public DateTime StartDate { get; set; }
+
+    [Column("end_date")]
+    public DateTime EndDate { get; set; }
+
+    [Column("status")]
+    public int Status { get; set; }
+
+    [Column("remarks", TypeName = "nvarchar(255)")]
+    public String Remarks { get; set; }
+
+    [Column("created_date")]
+    public DateTime CreatedDate { get; set; }
+
+    [Column("modified_date")]
+    public DateTime ModifiedDate { get; set; }
+}
